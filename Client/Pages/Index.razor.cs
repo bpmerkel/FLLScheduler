6725830ae461828b1d04898391e5b3757c35b850
Brady P. Merkel <brady@merkel.net>
2024-10-27 13:01:36 -0400

    Improve colors

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -647,6 +647,8 @@ public class RegistrationEntry
     public string Team { get; set; }
     public string Name { get; set; }
     public string Roster { get; set; }
+    public string Coach1 { get; set; }
+    public string Coach2 { get; set; }
 }
 
 public class TeamScheduleEntry
